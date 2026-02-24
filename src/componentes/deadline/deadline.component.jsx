9201f254647a1a6013c8f7829af4cf2e723a15f7
Erbi Guza <erbiguza@gmail.com>
2026-02-24 14:27:12 +0100

    bug fix

--- src/componentes/deadline/deadline.component.jsx
+++ src/componentes/deadline/deadline.component.jsx
@@ -9,6 +9,9 @@ function Deadline() {
                 <DeadlineTask duedate={"10/12/2026"} task={"task 1"} />
                 <DeadlineTask duedate={"10/3/2026"} task={"task 2"} />
                 <DeadlineTask duedate={"12/3/2026"} task={"task 3"} />
+                <DeadlineTask duedate={"10/12/2026"} task={"task 1"} />
+                <DeadlineTask duedate={"10/3/2026"} task={"task 2"} />
+                <DeadlineTask duedate={"12/3/2026"} task={"task 3"} />
             </div>
             <div className="button-container">
                 <button>View All Tasks</button>
